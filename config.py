faf094f446a220b240b56bd07881a94b82089665
sunil1342 <toletisunil123@gmail.com>
2025-03-17 18:21:48 -0400

    Update Config

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://user:password@localhost/carbon_credit_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
